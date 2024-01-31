c324d2073949b70b97a99a826ff417662cc63e7c
Shakil Khan <60468711+shakilkhan2045@users.noreply.github.com>
2024-01-31 11:34:26 +0600

    Update Array.java

--- Array.java
+++ Array.java
@@ -4,7 +4,7 @@ public class Array {
     public static void main(String[] args) {
      int[]  a = {10,20,30,40,50};
 
-     System.err.println( a[1] );
+     System.out.println( a[1] );
 
     }
 }
